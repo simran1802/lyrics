0c2b0bdad4ed59bbe5509249c6604186d2b2a8b0
Mohit Agarwal <Mohitagarwal9542@gmail.com>
2019-01-17 22:23:03 +0530

    correcting indentations

--- lyrics.py
+++ lyrics.py
@@ -12,18 +12,18 @@ elif len(sys.argv)==3:
 	song=sys.argv[2]
 else:
 	print("Usage: lyrics.py <artist(s)> <song>")
-        exit()
+	exit()
 
 doc = requests.get("http://lyrics.wikia.com/wiki/{0}:{1}".format(singer,song))
 if doc.status_code == 200:
 	soup = bs(doc.content,'html.parser')
 	lyricsdoc = soup.find_all("div", class_="lyricbox")
 	if len(lyricsdoc)<1:
-		print("Singer or song does not exist!")
+	    print("Singer or song does not exist!")
 	else:
-                s=str(lyricsdoc[0]).replace('<br/>','\n')
-		soup = bs(s,'html.parser')
-                print(soup.get_text())
+	    s=str(lyricsdoc[0]).replace('<br/>','\n')
+	    soup = bs(s,'html.parser')
+	    print(soup.get_text())
 
 else:
 	print("connection error")
